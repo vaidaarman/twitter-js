5c25f0b20e7a64399cd6a16529a6c1ad68870217
Vaida Armanaviciute <vaidaa@Vaidas-MacBook-Pro.local>
2018-01-16 10:09:04 -0600

    part 5 done

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const chalk = require('chalk');
 const volleyball = require('volleyball');
+const nunjucks = require('nunjucks');
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
@@ -25,12 +26,29 @@ app.get('/is-anybody-in-there', function(req, res, next) {
     res.send('Maybe... Why? Who\'s asking?');  
 });
 
-app.post('/modernism', function(req, res, next) {
-    res.send('Maybe... Why? Who\'s asking?');  
-});
+// var locals = {
+//     title: 'An Example',
+//     people: [
+//         { name: 'Gandalf'},
+//         { name: 'Frodo' },
+//         { name: 'Hermione'}
+//     ]
+// };
 
+// nunjucks.configure('views');
+// nunjucks.render('index.html', locals, function (err, output) {
+//     if (err) throw err;
+//     console.log(output);
+// });
 
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views'); // point nunjucks to the proper directory for templates
 
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+app.get('/views', function(req, res, next) {
+    res.render( 'index', {title: 'Hall of Fame', people: people} );
+})
 
 
 
